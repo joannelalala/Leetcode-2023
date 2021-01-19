1afd0ab67c73fbeeeb6ffe7eaff3c0bbe1999f6a
Jen <38231830+jenli810006995@users.noreply.github.com>
2021-01-19 08:25:36 -0500

    Create Merge_Two_Sorted_Lists.py

---
+++ Algorithm/Linked List/21. Merge Two Sorted Lists/Merge_Two_Sorted_Lists.py
@@ -0,0 +1,54 @@
+# Recursion
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 is None:
+            return l2
+        elif l2 is None:
+            return l1
+        elif l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+            
+# TC: O(M+N), the combination size of the linked lists
+# SC: O(M+N)
+
+# iteration
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        prehead = ListNode(-1)
+        
+        prev = prehead
+        while l1 and l2:
+            if l1.val <= l2.val:
+                prev.next = l1
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next
+            prev = prev.next
+        prev.next = l1 if l1 is not None else l2
+        return prehead.next
+        
+# TC:O(M+N)
+# SC:O(1) : The iterative approach only allocates a few pointers, so it has a constant overall memory footprint.
+
+# Link: https://leetcode.com/problems/merge-two-sorted-lists/
+
+        
+        
+
+
